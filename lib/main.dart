d25914e45dc458b52ea6cbf0b60626cbbf2860d9
Anchit Gupta <anchitgupt2012@gail.com>
2019-03-17 23:10:00 +0530

    Part1: Finished.
    Signed-off-by: Anchit Gupta <anchitgupt2012@gail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -7,16 +7,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: Text("Flutter App"),
-        ),
-        body: new Center(
-          child: new RandomWords()
-        ),
-      ),
+    return new MaterialApp(
+      title: 'Startup Name Generator',
+      home: new RandomWords(),
     );
   }
 }
@@ -27,11 +20,40 @@ class RandomWords extends StatefulWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
+
+  final List<WordPair> _suggestions = <WordPair>[];
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
   @override
   Widget build(BuildContext context) {
-    final List<WordPair> _suggestions = <WordPair>[];
-    final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
-    final WordPair wordPair = new WordPair.random();
-    return new Text(wordPair.asPascalCase);
+    return new Scaffold (
+      appBar: new AppBar(
+        title: new Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
   }
+
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (BuildContext _context, int i) {
+          if (i.isOdd) {
+            return const Divider();
+          }
+          final int index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+    Widget _buildRow(WordPair pair) {
+      return new ListTile(
+        title: new Text(
+          pair.asPascalCase,
+          style: _biggerFont,
+        ),
+      );
+    }
 }
